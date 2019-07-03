29b5959653a4c4ff8d8a16b32f00cc2ea987d464
Erik van Oosbree <erikvanoosbree@hotmail.com>
2019-07-03 11:25:17 +0200

    Create forms.py
    Added a form for geolocatethis

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DecimalField
+from wtforms.validators import DataRequired
+
+
+class geolocatethis(FlaskForm):
+    latitude = DecimalField('Latitude', validators=[DataRequired()])
+    longitude = DecimalField('Longitude', validators=[DataRequired()])
+    radius = DecimalField('Radius', validators=[DataRequired()])
+    keyword1  = StringField('Keyword for place 1', validators=[DataRequired()])
+    category1 = StringField('Category for place 1')
+    keyword2  = StringField('Keyword for place 2', validators=[DataRequired()])
+    category2 = StringField('Category for place 2')
+    distance = DecimalField('Distance between')
+    submit = SubmitField('search')
