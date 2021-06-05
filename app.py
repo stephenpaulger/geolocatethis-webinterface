051d085bacd563149fef285ee5dd253faa8c080c
Stephen Paulger <stephenpaulger@@users.noreply.github.com>
2021-06-05 13:21:29 +0100

    Results in template variable

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 from secrets import token_hex
 from forms import geolocatethisForm
 from geolocatethis import geo_locate_this
@@ -10,6 +10,7 @@ app.config["SECRET_KEY"] = token_hex(16)
 @app.route("/", methods=["GET", "POST"])
 def home():
     form = geolocatethisForm(request.form)
+    results = None
 
     if form.validate_on_submit():
         results = geo_locate_this(
@@ -23,10 +24,7 @@ def home():
             request.form["distance"],
         )
 
-        for hit in results:
-            flash(hit)
-
-    return render_template("index.html", form=form)
+    return render_template("index.html", form=form, results=results)
 
 
 @app.route("/about")
